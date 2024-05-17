856e615fc1e006ce5997cec83ef12d1b25d991f6
hirakvin <hirakvin@github.com>
2024-05-17 21:21:23 +0900

    branch synchronization

--- demo/fileOpen/global.h
+++ demo/fileOpen/global.h
@@ -2,25 +2,63 @@
 #ifndef __GlobalHeader
 #define __GlobalHeader
 
-// *** macro functions ***
-#define CTRL(c) ((c) & 037)  // macro function to read CTRL + character
+#include <curses.h>
+#include <time.h>
 
-// *** struct ***
+/* MACRO CONSTANT */
+// flag offset
+#define WINSIZE_CHANGED 0x0
+#define WAIT_ANY_INPUT 0x1
 
-typedef struct {
-	char *file_name;
-	char *full_path;
+/* STRUCT */
+
+typedef struct _file_status{
+	char file_name[256];
+	char full_path[256];
+    int fd;
     int row, col;
+    int modified; // 0 : unmodified  1 : tmp saved  2 : modified
+    time_t last_saved;
+    struct _file_status *next;
 } FileStatus; // saves opened file status - uses at file open & menu tab print ...
 
-// *** global variables ***
+typedef struct {
+    FileStatus *head;
+    int cnt;
+    FileStatus *focus;
+} OpenFileInfo;
 
-// file tab
-int max_file_tab; // cur window size determine this
-int file_tab_cnt;
-int file_tab_focus;
+enum MenuTab {
+    CODE_TAB,
+    FILE_TAB,
+    BUILD_TAB,
+    TERMINAL_TAB,
+    MANUAL_TAB,
+    QUIT_TAB,
+    WINSIZE_TAB
+};
 
-// menu tab
-int menu_tab_focus; // will define its value at menuTab.h
+/* GLOBAL VARIABLES */
 
+// global.c
+extern int winsize_flag; // 0 : default  1 : blocked  2 : ready to refresh
+
+extern enum MenuTab menu_tab_focus; // will define its value at menuTab.h
+extern enum MenuTab menu_tab_focus_backup[2];
+
+extern int win_row;
+extern int win_col;
+
+// uibase.h
+extern OpenFileInfo *opened_file_info;
+
+extern WINDOW *opened_file_tab;
+extern WINDOW *menu_tab;
+extern WINDOW *contents;
+
+#endif
+
+/* MACRO FUNCTIONS */
+#ifndef CTRL
+#define CTRL(c) ((c) & 037)// macro function to read CTRL + character
 #endif
